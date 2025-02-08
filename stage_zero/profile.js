9571829f94f4fda4a2e9ff22910763ae3514f632
Joy Ibe <139484063+DaFlusher@users.noreply.github.com>
2025-02-08 18:32:31 +0100

    Delete stage_zero directory

--- stage_zero/profile.js
+++
@@ -1,7 +0,0 @@
-function updateUTCTime() {
-    const utcTimeElement = document.getElementById('utc-time');
-    const now = new Date();
-    utcTimeElement.textContent = 'UTC Time: ' + now.toUTCString();
-}
-
-updateUTCTime();
